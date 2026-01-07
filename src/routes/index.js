8aef4f06eb122b2311cde37f7ccef4ad74c82096
Anoop RJ <arjanoop@gmail.com>
2026-01-07 17:31:30 +0100

    feat: add challenges model + controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const healthRoutes = require('./health.routes');
 const authRoutes = require('./auth.routes');
 const adminRoutes = require('./admin.routes');
+const challengeRoutes = require('./challenge.routes');
 
 const router = express.Router();
 
 router.use('/health', healthRoutes);
 router.use('/api/auth', authRoutes);
 router.use('/api/admin', adminRoutes);
+router.use('/api/challenges', challengeRoutes);
 
 module.exports = router;
